7be8bc7e635c444c0b04ce80bcb285781a4eedc8
Khushal <142389350+Anuragiskp@users.noreply.github.com>
2023-10-13 20:55:30 +0530

    Reaction implemented

--- index.js
+++ index.js
@@ -26,3 +26,48 @@ function addnewQualificationfield()
 
     qualOb.insertBefore(newNode, qualfieldaddbutton);
 }
+
+function generateCV()
+{
+    let namefield = document.getElementById("userName").value;
+
+    let nameT1 = document.getElementById("nameT1");
+    nameT1.innerHTML = namefield;
+
+    document.getElementById("nameT2").innerHTML = namefield;
+    document.getElementById("contactT").innerHTML = document.getElementById("Contact").value;
+    document.getElementById("addressT").innerHTML = document.getElementById("Address").value;
+    document.getElementById("EadressT").innerHTML = document.getElementById("Eaddress").value;
+    document.getElementById("LinkT").innerHTML = document.getElementById("LAddress").value;
+    document.getElementById("InsT").innerHTML = document.getElementById("IAddress").value;
+
+    document.getElementById("objectiveT").innerHTML = document.getElementById("object").value;
+     
+    //work field
+
+    let wes = document.getElementsByClassName("workfield");
+    let str="";
+
+    for(let e of wes)
+    {
+        str = str + `<li> ${e.value} </li>`;
+    }
+
+    document.getElementById("workexpT").innerHTML = str;
+
+    let quals = document.getElementsByClassName("workfield");
+    let btr="";
+
+    for(let e of btr)
+    {
+        str = str + `<li> ${e.value} </li>`;
+    }
+
+    document.getElementById("qualT").innerHTML = btr;
+
+}
+
+function printCV()
+{
+    
+}

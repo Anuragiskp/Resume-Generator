6623b4b083ff60f1586d0de3d6bc94d64857dfe4
Khushal <142389350+Anuragiskp@users.noreply.github.com>
2023-10-13 15:42:14 +0530

    buttons added

--- index.js
+++ index.js
@@ -12,3 +12,17 @@ function addnewField()
 
     weOb.insertBefore(newNode, workfieldaddButton);
 }
+
+function addnewQualificationfield()
+{
+    let newNode = document.createElement("textarea");
+    newNode.classList.add("form-control");
+    newNode.classList.add("AcaQualification");
+    newNode.classList.add("mt-2");
+    newNode.setAttribute("placeholder", "Enter here");
+
+    let qualOb = document.getElementById("AcaQual");
+    let qualfieldaddbutton = document.getElementById("QualButton");
+
+    qualOb.insertBefore(newNode, qualfieldaddbutton);
+}
